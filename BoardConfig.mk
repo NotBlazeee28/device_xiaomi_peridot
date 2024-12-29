9f50e8f84463b64b60f2ea518526cd2cf47f1455
kenway214 <kenway214@outlook.com>
2024-12-29 06:13:45 +0530

    peridot: Use media_profiles_cliffs_v0.xml as media.settings.xml
    media.settings.xml=/vendor/etc/media_profiles_cliffs_v0.xml
    
    * fixes 4k in miui camera and gcam
    
    Change-Id: I93a1fffab9771877de5aca30539e17ed1348e566
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: NotBlazeee28 <p.devansh@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,6 +174,7 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/props/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
 
 # Recovery
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
