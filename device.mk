amiami5/android_device_xiaomi_peridot

peridot: Build sensors.xiaomi.v2
Change-Id: If76c6d84e966b34247bcf9b7a77f31d87de6a538
Signed-off-by: lollipuru <banstellazz@gmail.com>

device.mk
@@ -503,7 +503,11 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.xiaomi-multihal
+    android.hardware.sensors-service.xiaomi-multihal \
+    sensors.xiaomi.v2
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_ODM)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge \

NotBlazeee28/device_xiaomi_peridot

Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

device.mk
@@ -502,7 +502,11 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.xiaomi-multihal
+    android.hardware.sensors-service.xiaomi-multihal \
+    sensors.xiaomi.v2
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge \